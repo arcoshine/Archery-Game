97a6de24476553ad09ada5c569ca3ba4e891af92
BBBNBBB <benjamin@munster.me.uk>
2022-08-23 12:24:42 +0100

    changes velocity due to acceration & adjust displacement calc

--- Archery Game/Assets/ArrowFly.cs
+++ Archery Game/Assets/ArrowFly.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class ArrowFly : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Vector3 velos;
-    private Vector3 accels;
+    private Vector3 velos; //velocity in each direction
+    private Vector3 accels; //acceleration in each direction
+    private Vector3 gravFor; //force downwards due to gravity
+    public int gravity; //value of g
+    private Vector3 dragFors; //forces in each direction
     void Start()
     {
         velos = Vector3.Normalize(transform.TransformVector(Vector3.forward)) * 2.0f; //set vector for arrow movement at 2m/s initially
@@ -15,9 +18,9 @@ public class ArrowFly : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate((velos * Time.deltaTime), Space.World); //move the arrow at correct velocities for time between movements
-        Debug.Log(velos * Time.deltaTime); //check how far the arrow moved
-        Debug.Log(velos); //output velocities after normalisation
-        Debug.Log(Time.deltaTime); //check how far the arrow moved
+        velos = velos + (Time.deltaTime * accels); //calculate new velocity given acceleraction
+        var displacement = velos * Time.deltaTime - (0.5f * accels * Mathf.Pow(Time.deltaTime, 2.0f)); //calculate the displacement in the given period
+        transform.Translate(displacement, Space.World); //move the arrow at correct velocities for time between movements
+        Debug.Log(displacement); //check how far the arrow moved
     }
 }
