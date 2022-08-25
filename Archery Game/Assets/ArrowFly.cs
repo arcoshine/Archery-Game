d0dcedc1e106b7905ed40f2c936032d6a7da09f1
BBBNBBB <benjamin@munster.me.uk>
2022-08-25 12:14:52 +0100

    changes the initial velocity of the arrow based on button time held

--- Archery Game/Assets/ArrowFly.cs
+++ Archery Game/Assets/ArrowFly.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class ArrowFly : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Vector3 velos; //velocity in each direction
-    private Vector3 accels; //acceleration in each direction
-    private Vector3 gravFor; //force downwards due to gravity
-    public int gravity; //value of g
-    private Vector3 dragFors; //forces in each direction
+    public Vector3 velos; //velocity in each direction
+    public Vector3 accels; //acceleration in each direction
+    public Vector3 gravFor; //force downwards due to gravity
+    public float gravity = 9.8f; //value of g
+    public Vector3 dragFors; //forces in each direction
+    private FireArrow fireArrow; //used to reference FireArrow script
+    public GameObject camera_m; //used to reference the Main Camera
+    private float timeDown; //used to represent time that fire 1 was held down
     void Start()
     {
-        velos = Vector3.Normalize(transform.TransformVector(Vector3.forward)) * 2.0f; //set vector for arrow movement at 2m/s initially
+        camera_m = GameObject.FindWithTag("MainCamera"); //get reference to the main camera
+        fireArrow = camera_m.GetComponent<FireArrow>(); //get reference to FireArrow
+        timeDown = fireArrow.timeHeld; //Fire timeHeld
+        velos = Vector3.Normalize(transform.TransformVector(Vector3.forward)) * 10.0f; //set vector for arrow movement at 2m/s initially
+        velos = Mathf.Min(Mathf.Sqrt(timeDown), 20.0f) * velos; //adjust 
     }
 
     // Update is called once per frame
@@ -21,6 +28,10 @@ public class ArrowFly : MonoBehaviour
         velos = velos + (Time.deltaTime * accels); //calculate new velocity given acceleraction
         var displacement = velos * Time.deltaTime - (0.5f * accels * Mathf.Pow(Time.deltaTime, 2.0f)); //calculate the displacement in the given period
         transform.Translate(displacement, Space.World); //move the arrow at correct velocities for time between movements
-        Debug.Log(displacement); //check how far the arrow moved
+        //Debug.Log(velos); //check how far the arrow moved
+    }
+    public void getTimeHeld(float timeDown)
+    {
+        //Debug.Log(velos + "getTimeHeld");
     }
 }
--- Archery Game/Assets/FireArrow.cs
+++ Archery Game/Assets/FireArrow.cs
@@ -6,6 +6,8 @@ public class FireArrow : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject objFired;
+    public float timeHeld; //time that the left click is held
+    public float timeStart; //the time the left click button is pressed down
     void Start()
     {
         
@@ -14,12 +16,17 @@ public class FireArrow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")) //when the Fire1 key (left click) is pressed
+        if(Input.GetButtonDown("Fire1"))
         {
-            SpawnArrow(); //spawn an arrow
+            timeStart = Time.time;
+        }
+        if(Input.GetButtonUp("Fire1")) //when the Fire1 key (left click) is pressed
+        {
+            timeHeld = Time.time - timeStart;
+            SpawnArrow(timeHeld); //spawn an arrow
         }
     }
-    void SpawnArrow()
+    void SpawnArrow(float drawTime)
     {
         //spawn the arrow
         var arrowNew = Instantiate(objFired, (transform.position + transform.forward), transform.rotation); //create object 1 unit infront of viewe
